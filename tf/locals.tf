8f55fb2c4cec605e987ac9c88daacd7f1363f834
chrispsheehan <chrispsheehan@gmail.com>
2024-11-14 12:53:12 +0000

    fmt

--- tf/locals.tf
+++ tf/locals.tf
@@ -3,14 +3,14 @@ locals {
   az_count            = min(length(data.aws_availability_zones.azs.names), var.max_az)
   cloudwatch_log_name = "/ecs/${local.formatted_name}"
   container_definitions = templatefile("${path.module}/container_definitions.tpl", {
-    container_name           = var.project_name
-    image_uri                = data.aws_ecr_image.latest_image.image_uri
-    container_port           = var.container_port
-    cpu                      = var.cpu
-    memory                   = var.memory,
-    aws_region               = var.region,
-    api_key_ssm_param_name   = var.api_key_ssm_param_name,
-    cloudwatch_log_name      = local.cloudwatch_log_name
+    container_name         = var.project_name
+    image_uri              = data.aws_ecr_image.latest_image.image_uri
+    container_port         = var.container_port
+    cpu                    = var.cpu
+    memory                 = var.memory,
+    aws_region             = var.region,
+    api_key_ssm_param_name = var.api_key_ssm_param_name,
+    cloudwatch_log_name    = local.cloudwatch_log_name
   })
 }
  
