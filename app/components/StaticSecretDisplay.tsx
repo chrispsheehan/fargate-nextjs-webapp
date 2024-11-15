3ef468264122b60fa2cb8cf44cb5078900064253
chrispsheehan <chrispsheehan@gmail.com>
2024-11-15 17:04:03 +0000

    frontend props note

--- app/components/StaticSecretDisplay.tsx
+++ app/components/StaticSecretDisplay.tsx
@@ -8,7 +8,7 @@ interface StaticSecretDisplayProps {
 const StaticSecretDisplay: React.FC<StaticSecretDisplayProps> = ({ secret }) => {
   return (
     <div style={{ padding: '1em', border: '1px solid #ccc', borderRadius: '8px', width: 'fit-content', marginTop: '1em' }}>
-      <h2>Static Secret Display</h2>
+      <h2>Static Secret Display using getServerSideProps()</h2>
       <h3>Only first and last characters are shown</h3>
       <p style={{ fontWeight: 'bold', color: '#333', fontSize: '1.2em' }}>{secret || 'Secret not specified'}</p>
     </div>
